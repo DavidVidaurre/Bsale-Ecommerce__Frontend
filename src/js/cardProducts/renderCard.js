f5d46760389ea0db41dfeaf52514a72196212e9d
DavidVidaurre <luisdavidmaquenvidaurre@gmail.com>
2022-12-23 12:01:34 -0500

    Mejorando estilos en el carrito de compras y los filtros desktop

--- src/js/cardProducts/renderCard.js
+++
@@ -1,8 +0,0 @@
-import productCard from './productCard.js';
-
-const renderCard = (product) => {
-    const hasDiscount = product.discount !== 0 ? true : false;
-    document.getElementById('productCardsContainer').innerHTML += productCard(product, hasDiscount);
-}
-
-export default renderCard
